eb5f5ee9ce5f9e1ee5438382d8c31e47542a8f13
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-04-23 05:29:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Educore
+Educore serves to centralize educational software data, allowing systems to easily integrate with each other.
+
+
---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: process.env.NODE_ENV != 'development' && !process.env.CI
+		},
+		csp: {
+			directives: {
+				'script-src': ['self']
+			},
+			reportOnly: {
+				'script-src': ['self'],
+				'report-to': ['csp-endpoint']
+			}
+		}
+	}
+};
+
+export default config;
