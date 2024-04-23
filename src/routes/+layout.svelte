55ddee5b61bdc1653dafc71eca1d437c0bd9a234
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-04-23 05:35:40 -0400

    basic styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '@fontsource/open-sauce-one';
+</script>
+
+<div class="wrap">
+	<slot/>
+</div>
+
+
+<style lang="scss">
+	:global(*) {
+
+		box-sizing: border-box;
+		font-family: 'Open Sauce One', sans-serif;
+	}
+	:global(body) {
+		margin: 0px;
+		display: flex;
+		height: 100vh;
+		width: 100%;	
+	}
+
+	.wrap {
+		width: 100%;
+		height: 100%;
+	}
+
+</style>
